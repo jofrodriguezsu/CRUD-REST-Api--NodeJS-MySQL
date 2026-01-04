0dc3405e90845d58eb5ad7ee128c9ad73007e257
ichthus <jofrodriguezsu@gmail.com>
2026-01-04 14:34:39 -0500

    model

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+const db = require ("../config/db.js")
+
+const createUser = async (name, email) => {
+  const [result] = await db.execute(
+    "INSERT INTO users (name, email) VALUES (?,?)",
+    [name, email]
+  )
+  return result.insertId
+}
+
+const getAllUsers = async () => {
+  const [rows] = await db.execute(
+    "SELECT * FROM users"
+  )
+  return rows
+}
+
+const updateUser = async (id, name, email) => {
+  const [result] = await db.execute(
+    "UPDATE users SET name =?, email =?, WHERE id =?"
+    [id, name, email]
+  )
+}
+
+const deleteUser = async (id) => {
+  const [result] = await db.execute(
+    "DELETE FROM users WHERE id =?", [id]
+  )
+}
+
+module.exports = {
+  createUser,
+  getAllUsers,
+  updateUser,
+  deleteUser
+}
